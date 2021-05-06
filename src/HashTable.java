3471b4a1d21f80a454c101556b56ba65ba8ac6c0
MDerv <dsharma21@kentdenver.org>
2021-05-06 00:24:25 -0600

    I fixed the code and added a driver to run the program.

--- src/HashTable.java
+++ src/HashTable.java
@@ -5,40 +5,42 @@ public class HashTable {
 
     //initializes an array of size capacity
     public HashTable(int capacity) {
-        //List<ArrayList<String>> HashTable2DArr = new ArrayList<ArrayList<String>>(capacity);
         HashTableArr = new ArrayList(capacity);
+        for (int i = 0; i < capacity; i++) {
+            HashTableArr.add(null);
+        }
     }
 
     //put hashes the key to an index in your array, and places the value there. Fails if there are collisions/repeat keys.
     public boolean put(String key, String value) {
-        for(int i = 0; i <= HashTableArr.size(); i+=2) {
-            if(HashTableArr.get(i).equals(key)) {
-                return false;
-            }
+        String indexNum = "";
+
+        for (int i = 0; i < key.length(); i++) {
+            indexNum += (int)key.charAt(i);
         }
-        HashTableArr.add(key);
-        HashTableArr.add(value);
-        return true;
+
+        if (HashTableArr.get(Integer.parseInt(indexNum)%601) == null) {
+            HashTableArr.add(Integer.parseInt(indexNum)%601, value);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     //get hashes the key to get the index, and returns that element. Returns null if key not found.
     public String get(String key) {
-        for (int i = 0; i <= HashTableArr.size(); i += 2){
-            if(HashTableArr.get(i).equals(key)) {
-                return HashTableArr.get(i+1);
-            }
-        }
-        return null;
+        return HashTableArr.get(this.hashCode(key));
     }
 
     //returns the unique int in the range of the [0, array length)
     private int hashCode(String key) {
-        for (int i = 0; i <= HashTableArr.size(); i += 2){
-            if(HashTableArr.get(i).equals(key)) {
-                return i;
-            }
-        }
-    return -1;
-    }
+        String indexNum = "";
 
+        for (int i = 0; i < key.length(); i++) {
+            indexNum += (int)key.charAt(i);
+        }
+
+        return Integer.parseInt(indexNum)%601;
+    }
 }
--- src/HashTableDriver.java
+++ src/HashTableDriver.java
@@ -1,2 +1,17 @@
 public class HashTableDriver {
+    public static void main(String[] args) {
+        HashTable h = new HashTable(10000);
+
+        //putting value into Hashmap (Output: true)
+        System.out.println(h.put("hi", "hola"));
+
+        //should fail (Output: false)
+        System.out.println(h.put("hi", "adios"));
+
+        //print key's value (Output: hola)
+        System.out.println(h.get("hi"));
+
+        //should return null (Output: null)
+        System.out.println(h.get("lol"));
+    }
 }
